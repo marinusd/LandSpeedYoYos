1a1f3d47d7b9be442971e82d661ce45e78f473ec
marinusd <github@plesba.com>
2013-07-19 19:20:26 -0700

    Also implement gps status listener

--- src/com/plesba/ioio_logger/GPS_ListenerService.java
+++ src/com/plesba/ioio_logger/GPS_ListenerService.java
@@ -3,11 +3,13 @@ package com.plesba.ioio_logger;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
 
 public class GPS_ListenerService extends Service {
@@ -52,13 +54,36 @@ public class GPS_ListenerService extends Service {
 		locationManager.removeUpdates(gpsLocationListener);
 	}
 
-	private class GPSLocationListener implements LocationListener {
+	private class GPSLocationListener implements LocationListener, GpsStatus.Listener {
+		Location mLastLocation = null;
+		boolean isGPSFix;
+		
+	    public void onGpsStatusChanged(int event) {
+	        switch (event) {
+	            case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+	                if (mLastLocation != null)
+	                    isGPSFix = (SystemClock.elapsedRealtime() - lastGPStime) < 3000;
+	                if (isGPSFix) { // A fix has been acquired.
+	                	Log.i(TAG, "GPS has a fix.");
+	                } else { // The fix has been lost.
+	                	Log.w(TAG, "GPS DOES NOT have a fix.");
+	                }
+	                break;
+	            case GpsStatus.GPS_EVENT_FIRST_FIX:
+                	Log.i(TAG, "GPS got first fix.");
+	                isGPSFix = true;
+	                break;
+	        }
+	    }
+	    
 		@Override
 		public void onLocationChanged(Location location) {
+			mLastLocation = location;
 			lastGPStime   = location.getTime();
 			lastLatitude  = location.getLatitude();
 			lastLongitude = location.getLongitude();
 			lastSpeed     = location.getSpeed();
+			Log.i(TAG, "GPS update received.");
 		}
 
 		@Override
